87698f2422d3d296c26d6bfcaa62525006384e9b
Bruno Costa <bruno.costa@superlogica.com>
2022-04-20 13:51:38 -0300

    categoria invalida

--- tests/Feature/UseCases/Genres/CreateGenreUseCaseTest.php
+++ tests/Feature/UseCases/Genres/CreateGenreUseCaseTest.php
@@ -7,19 +7,18 @@ use App\Repositories\Eloquent\GenreRepository as Repository;
 use App\Models\Genre as Model;
 use App\Repositories\Eloquent\CategoryRepository;
 use App\Repositories\Transactions\TransactionDatabase;
+use Costa\Core\Domains\Exceptions\NotFoundDomainException;
+use Costa\Core\Domains\ValueObject\Uuid;
 use Costa\Core\UseCases\Genre\CreateGenreUseCase as UseCase;
 use Costa\Core\UseCases\Genre\DTO\Created\Input;
 use Tests\TestCase;
 
 class CreateGenreUseCaseTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_create()
+    public function testCreate()
     {
+        $categories = Category::factory(4)->create()->pluck('id')->toArray();
+
         $repo = new Repository(new Model);
 
         $useCase = new UseCase(
@@ -29,7 +28,8 @@ class CreateGenreUseCaseTest extends TestCase
         );
 
         $response = $useCase->execute(new Input(
-            name: 'teste'
+            name: 'teste',
+            categories: $categories
         ));
 
         $this->assertEquals('teste', $response->name);
@@ -39,5 +39,28 @@ class CreateGenreUseCaseTest extends TestCase
             'id' => $response->id,
             'name' => $response->name,
         ]);
+
+        $this->assertDatabaseCount('category_genre', 4);
+    }
+
+    public function testCreateCategoriesInvalids()
+    {
+        $id = (string) Uuid::random();
+
+        $this->expectException(NotFoundDomainException::class);
+        $this->expectExceptionMessage("Category {$id} not found");
+
+        $repo = new Repository(new Model);
+
+        $useCase = new UseCase(
+            repository: $repo,
+            transactionContract: new TransactionDatabase(),
+            categoryRepositoryInterface: new CategoryRepository(new Category())
+        );
+
+        $useCase->execute(new Input(
+            name: 'teste',
+            categories: [$id]
+        ));
     }
 }
